6a2402dd1c76a8c222fa6e71469b186cd150cd89
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-05-04 22:22:34 +0600

    sdfs

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const res = require("express/lib/response");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://hamid42:7JwG2D8fNJNdprtm@cluster0.1geox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1geox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
