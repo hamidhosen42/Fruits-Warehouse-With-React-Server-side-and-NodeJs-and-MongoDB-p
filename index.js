02b5ca35f9a16e49890f8cc1118ef366ff614ef7
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-05-04 22:42:47 +0600

    final

--- index.js
+++ index.js
@@ -24,7 +24,95 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db("fruits").collection("service");
 
+    // AUTH
+    //require('crypto').randomBytes(64).toString('hex')
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
+    //load data inventory API-----
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const service = await cursor.limit(6).toArray();
+      res.send(service);
+    });
+
+    // update item
+    app.put("/updateitem/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updateUser.name,
+          suplierName: updateUser.suplierName,
+          price: updateUser.price,
+          quantity: updateUser.quantity,
+          img: updateUser.img,
+          description: updateUser.description,
+        },
+      };
+      const result = await serviceCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    //inventory details
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //load data updateitem API-----
+    app.get("/updateitem/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // My ITEM API
+    app.get("/myitem", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = serviceCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    // add item
+    app.post("/additem", async (req, res) => {
+      const order = req.body;
+      const result = await serviceCollection.insertOne(order);
+      res.send(result);
+    });
+
+    // manage all itemm
+    app.get("/manageitem", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const service = await cursor.toArray();
+      res.send(service);
+    });
+
+    //  manageinventorys all item
+    app.get("/manageinventorys", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const service = await cursor.toArray();
+      res.send(service);
+    });
 
     // DELETE manageinventory
     app.delete("/manageinventorys/:id", async (req, res) => {
